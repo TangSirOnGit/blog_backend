8bcadfafffce7e11f792b3309087b4ada5d3da44
laimingjiang <lai.mingjiang@bestv.com.cn>
2023-02-23 12:20:29 +0800

    first commit

---
+++ models/blog.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
